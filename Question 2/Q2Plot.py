6e7548ffbd1e69ff2ee5f40f4708cfc9994ef1b4
Dion Calim <145569211+Dcalim@users.noreply.github.com>
2024-04-01 17:58:14 -0400

    DONE

--- Question 2/Q2Plot.py
+++ Question 2/Q2Plot.py
@@ -21,29 +21,39 @@ def main(argv):
     #data fields read from csv file
     df = pd.read_csv(fileName)
 
-    # Runs if any data fields in csv file matches year and province inputted by user
-    if ((df['REF_DATE'].str[:4] == year) & (df['GEO'] == province)).any():
-        csvYear = df[(df['REF_DATE'].str[:4] == year) & (df['GEO'] == province)]['REF_DATE']
-        csvValue = df[(df['REF_DATE'].str[:4] == year) & (df['GEO'] == province)]['VALUES'] 
-        print("Printing csvYear\n")
-        print(csvYear)
-        print("Finished Printing csvYear\n\n")
+    # Runs if any data fields in csv file matches the province inputted by user
+    if ((df['GEO'] == province)).any():
+        
+        # Runs if any data fields in csv file matches the year inputted by user
+        if((df['REF_DATE'].str[:4] == year)).any():
+            csvYear = df[(df['REF_DATE'].str[:4] == year) & (df['GEO'] == province)]['REF_DATE']
+            csvValue = df[(df['REF_DATE'].str[:4] == year) & (df['GEO'] == province)]['VALUES'] 
+            print("Printing csvYear\n")
+            print(csvYear)
+            print("Finished Printing csvYear\n\n")
 
-        print("Printing csvValues")
-        print(csvValue)
-        print("Done Printing csvValues\n")
+            print("Printing csvValues")
+            print(csvValue)
+            print("Done Printing csvValues\n")
 
-        plt.title(province + " average monthly earning in " + year)
-        plt.xlabel("Months")
-        plt.ylabel("Average Earning in Dollars")
-        plt.plot(months, csvValue)
-        manager = plt.get_current_fig_manager()
-        manager.full_screen_toggle()
-        plt.show()
+            plt.title(province + " average monthly earning in " + year, fontweight='bold')
+            plt.xlabel("Months", fontweight='bold')
+            plt.ylabel("Average Earning in Dollars", fontweight='bold')
+            plt.plot(months, csvValue)
     
-    # Runs if it cannot find any date fields that match the year or province inputted by user
+            # Showing plot on full seperate screen
+            manager = plt.get_current_fig_manager()
+            manager.full_screen_toggle()
+            plt.show()
+        
+        # Runs if it cannot find the year inputted by user
+        else:
+            print("Could not find any data for that year.")
+
+    
+    # Runs if it cannot find the province inputted by user
     else:
-        print("No data found for the specified province and year.")
+        print("No data found for the specified province, please ensure that the province is beginning with a capital.")
         sys.exit()
 
 main(sys.argv)
